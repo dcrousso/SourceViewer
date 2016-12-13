2377ff8e42e0a20b6f8d20fb7f17104cced1b103
Devin Rousso <dcrousso@gmail.com>
2016-12-12 23:14:38 -0700

    Initial Commit

---
+++ Safari/SourceViewer.safariextension/html/js/viewer.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const URL = window.location.search.substr(1);
+
+document.title = `view-source:${URL}`;
+
+fetch(URL)
+.then(response => response.text())
+.then(text => {
+	CodeMirror(document.body, {
+		cursorBlinkRate: -1,
+		lineNumbers: true,
+		lineWrapping: true,
+		readOnly: true,
+		value: text,
+	});
+});
